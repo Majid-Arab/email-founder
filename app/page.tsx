8711d2eb6672e6874212e1b866897369ae28976e
Majid-Arab <arabmajid12@gmail.com>
2023-11-06 23:52:35 +0500

    this is the begining of Surprise project.
    just created the form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { HomeForm } from "../components/Form";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <div style={{width: "80%", margin: "auto"}}>
+      <HomeForm />
+    </div>
+  );
 }
