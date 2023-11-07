58421d577c9861b4120702634f3ba7516c6dd140
Majid-Arab <arabmajid12@gmail.com>
2023-11-07 17:27:28 +0500

    centered form div horizontally

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,14 @@ import { HomeForm } from "../components/Form";
 
 export default function HomePage() {
   return (
-    <div style={{width: "80%", margin: "auto"}}>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
       <HomeForm />
     </div>
   );
